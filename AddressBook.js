3f6ebe5779be464085a84f38b788fb521b961a94
Krishna <krishnaag866@gmail.com>
2025-03-21 11:41:08 +0530

    display total contacts

--- AddressBook.js
+++ AddressBook.js
@@ -42,6 +42,9 @@ class AddressBook {
     this.contacts=this.contacts.filter(c=>c.firstName!==firstName && c.lastName!==lastName);
     return (initialLength>this.contacts.length)?"contact deleted":"contact Not Found";
   }
+  totalContacts(){
+    return this.contacts.reduce(count=>count+1,0);
+  }
   listContacts(){
   return this.contacts;
   }
@@ -54,6 +57,7 @@ try {
 } catch (error) {
   console.error(error.message);
 }
+console.log(addressBook.totalContacts());
 console.log(addressBook.deleteContact("Juhi","Agarwal"));
 console.log(addressBook.editContact("Krihs", "Doe",{phone:"2341854107", city:"Chicago"}));
 console.log(addressBook.listContacts());
