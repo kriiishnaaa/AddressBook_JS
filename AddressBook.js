0f02cb1378e1ad318811de587e4120293f3d4f77
Krishna <krishnaag866@gmail.com>
2025-03-21 10:58:17 +0530

    created address book in Javascript

---
+++ AddressBook.js
@@ -0,0 +1,12 @@
+class Contact{
+constructor(firstName, lastName, address, city, zip, state, phnNumber, email){
+this.firstName=firstName;
+this.lastName=lastName;
+this.address=address;
+this.city=city;
+this.zip=zip;
+this.state=state;
+this.phnNumber=phnNumber;
+this.email=email;
+}
+}
